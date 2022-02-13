a4c36f9d8b471fad09cc6c689b7758b82c6c060b
Eric Shumway <ericshumway2@mail.weber.edu>
2022-02-12 19:02:39 -0700

    finalizing and cleaning up script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,67 @@
 //assignment code goes here
 var password = document.getElementById("password");
+//establishes the variables possible for the string of characters
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberchar = "0123456789";
+var specialCharacters = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+//function to build the password.
 function generatePassword() {
+  //get password length
   var passwordLength = window.prompt("How many characters would you like to have?");
-  var capitalChar = window.confirm("Would you like captial letters?");
-  var specialChar = window.confirm("Would you like special symbols?");
-  var numChar = window.confirm("Would you like Numbers?");
-
-  if (capitalChar && specialChar && numChar) {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var password = "";
-    for (var i = 1; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-
-    }
-  }
-  
-  if (capitalChar && specialChar && !numChar) {
-    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var password = "";
-    for (var i = 1; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-
-    }
-  }
-  
-  if (capitalChar  && numChar && !specialChar) {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var password = "";
-    for (var i = 1; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-
-    }
+  // validate password length is not left empty
+  if (passwordLength === "" || passwordLength === null) {
+    generatePassword();
   }
 
+  else {
+    //validate passwordLength is between 8 and 128
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      var charOptions = "";
+      var lowerChar = window.confirm("Would you like to have lowercase letters?");
+      var capitalChar = window.confirm("Would you like captial letters?");
+      var specialChar = window.confirm("Would you like special symbols?");
+      var numChar = window.confirm("Would you like Numbers?");
+      //adds the lowercase to the character options
+      if (lowerChar) {
+        charOptions = charOptions + lowercase;
+      }
+      //adds the uppercase to the character options
+      if (capitalChar) {
+        charOptions = charOptions + capitalLetters;
+      }
+      //adds the special characters to the character options
+      if (specialChar) {
+        charOptions = charOptions + specialCharacters;
+      }
+      //adds the numbers to the character options
+      if (numChar) {
+        charOptions = charOptions + numberchar;
+      }
+      //makes sure that one of the options was chosen by the user.
+      if (!capitalChar && !specialChar && !numChar && !lowerChar) {
+        generatePassword();
+      }
+      // randomly selects the Characters for the password.
+      if (passwordLength >= 8 && passwordLength <= 128) {
+        var password = "";
+        for (var i = 1; i <= passwordLength; i++) {
+          var randomNumber = Math.floor(Math.random() * charOptions.length);
+          password += charOptions.substring(randomNumber, randomNumber + 1);
 
-  if (capitalChar && !specialChar && !numChar) {
-    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var password = "";
-    for (var i = 1; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-
+        }
+      }
     }
-  }
-  
-
-
-  
-
-  if (specialChar && numChar && !capitalChar) {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
-    var password = "";
-    for (var i = 1; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-
-    }
-  }
-
-  if (specialChar && !numChar && !capitalChar) {
-    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
-    var password = "";
-    for (var i = 1; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-
-    }
-  }
-
-
-  if (numChar && !specialChar && !capitalChar) {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz";
-    var password = "";
-    for (var i = 1; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-
-    }
-  }
-  if (!specialChar && !numChar && !capitalChar) {
-    var chars = "abcdefghijklmnopqrstuvwxyz";
-    var password = "";
-    for (var i = 1; i <= passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber + 1);
-
-    }
-  }
-
-
-  
-
-
-  
     
-  
-  return password;
-  debugger;
+    else {
+      generatePassword();
+    }
+
+    return password;
+
+  }
 }
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -117,19 +76,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-/*var password = document.getElementById("#password");
-function generatePassword() {
-  
-var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
-  var password = "";
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber + 1);
-  }
-  document.getElementById("#password").value = password;
-}*/
